8533b6a88a6b45109cbdcbf37704b849b74cb166
selamawit haileselasse <139425406+selamawit21-tsige@users.noreply.github.com>
2025-03-15 10:03:42 -0700

    inital react

---
+++ src/components/hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hello() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Hello, React World! 👋</h1>
+      <p>This is your first React component.</p>
+    </div>
+  );
+}
+
+export default Hello;
